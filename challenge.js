df687574ce119d5e7ec7f276cd4380566ef39b13
Abdulkadir1614 <abdukadir1614@gmail.com>
2024-09-07 18:10:05 +0800

    Done 😃

--- challenge.js
+++ challenge.js
@@ -1,2 +1,35 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculateBMI(weight, height){
+  return weight / (height * height);
+}
+// Ali's BMI
+
+const Aliweight = 70
+const Aliheight = 1.65
+
+const AliBMI = calculateBMI(Aliweight, Aliheight)
+
+// Mohamed's BMI
+
+const Mohamedweight = 80
+const Mohamedheight = 1.70
+
+const MohamedBMI = calculateBMI(Mohamedweight, Mohamedheight)
+
+//compare 
+
+if (AliBMI > MohamedBMI){
+    console.log ("Ali has higher BMI than Mohamed")
+}
+else if (AliBMI < MohamedBMI){
+    console.log ("Mohamed has higher BMI than Ali")
+}
+else {
+    console.log("Both Mohamed and Ali has same BMI")
+}
+
+console.log(`Ali's BMI ${AliBMI}`)
+console.log(`Mohamed's BMI ${MohamedBMI}`)
+
